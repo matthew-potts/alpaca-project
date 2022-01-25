f6a92e310d157b957c42008b1cd8ac560c9eeae1
matthew-potts <matthew-potts@live.com>
2022-01-25 21:44:03 +0000

    working locally, not when deployed to aws

--- alpaca/app.py
+++ alpaca/app.py
@@ -1,29 +1,33 @@
 from chalice import Chalice
+# import requests
+# from bs4 import BeautifulSoup
 
 app = Chalice(app_name='alpaca')
-
 
 @app.route('/')
 def index():
     return {'hello': 'world'}
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/oversold')
+def get_stock(): # gets oversold URL by default; can also switch to overbought_url
+
+    API_KEY = 'Your API-KEY Here'
+    SECRET_KEY = 'Your SECRET-KEY Here'
+    USER_AGENT = 'Your User-Agent Here'
+    HEADERS = {
+        'APCA-API-KEY-ID': API_KEY,
+        'APCA-API-SECRET-KEY': SECRET_KEY
+    }
+
+    BASE_URL = 'https://paper-api.alpaca.markets'
+    ACCOUNT_URL = '{}/v2/account'.format(BASE_URL)
+    ORDERS_URL = '{}/v2/orders'.format(BASE_URL)
+    POSITIONS_URL = '{}/v2/positions'.format(BASE_URL)
+
+    oversold_url = 'https://finviz.com/screener.ashx?v=111&s=ta_oversold&f=exch_nasd'
+    headers = {'User-Agent': USER_AGENT}
+    r = requests.get(oversold_url, headers = headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    rows = soup.find_all('a', {'class': 'screener-link-primary'})
+    return [row.text for row in rows]
